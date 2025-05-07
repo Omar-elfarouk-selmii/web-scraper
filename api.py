893d7bac4deeeb6e76bf9579ce42c15d8fbbd1e6
Omar-elfarouk-selmii <omarselmi374@gmail.com>
2025-05-07 01:00:46 +0100

    performed many changes (styles , ui)

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, send_file, request,render_template
-from scraper import scrape_quotes,scrape_any_website
+from flask import Flask, jsonify, send_file, request, render_template
+from scraper import scrape_quotes, scrape_any_website
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import os
@@ -14,30 +14,18 @@ limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["200 per
 # Cache scraped data
 QUOTES_DATA = scrape_quotes()
 pprint(QUOTES_DATA)
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def get_site():
     return render_template("index.html")
-
 
 @app.route('/quotes', methods=['post'])
 @limiter.limit("10 per minute")
 def get_all_quotes():
     """Returns all scraped quotes as JSON."""
-    url=request.form['url']
+    url = request.form['url']
     QUOTES_DATA = scrape_quotes(url)
     return jsonify(QUOTES_DATA)
-
-@app.route('/quotes/<string:author>', methods=['GET'])
-@limiter.limit("10 per minute")
-def get_quotes_by_author(author):
-    """Returns quotes for a specific author (case-insensitive)."""
-    if not author or len(author) > 50:  # Input validation
-        return jsonify({"message": "Invalid author name."}), 400
-    filtered = [q for q in QUOTES_DATA if q['author'].lower() == author.lower()]
-    if filtered:
-        return jsonify(filtered)
-    else:
-        return jsonify({"message": "No quotes found for this author."}), 404
 
 @app.route('/scrape', methods=['POST'])
 def scrape_url():
@@ -47,15 +35,10 @@ def scrape_url():
         return jsonify({"message": "URL is required."}), 400
     
     url = data['url']
-    if url=="http://quotes.toscrape.com":
+    if url == "http://quotes.toscrape.com":
         quotes = scrape_quotes(url)
         return jsonify(quotes)
     else:
-        # print('"""""""""""""""')
-
-        # url=request.form.get('url')
-        # print(url)
-        # print('"""""""""""""""')
         try:
             quotes = scrape_any_website(url)
             return jsonify(quotes)
